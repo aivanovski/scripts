b88367e048de83c4f319a0b6a0a745bba47f64b5
Aleksey Ivanovsky <alexei.ivanovski@gmail.com>
2022-03-27 20:43:29 +0200

    Update config-menu.rb

--- config-menu.rb
+++ config-menu.rb
@@ -8,12 +8,14 @@ BASHRC = "~/.bashrc"
 VIMRC = "~/.vimrc"
 I3_CONFIG = "~/.config/i3/config"
 POLYBAR_CONFIG = "~/.config/polybar/config.ini"
+NVIM_CONFIG = "~/.config/nvim"
 
 options = [
     BASHRC,
     VIMRC,
     I3_CONFIG,
-    POLYBAR_CONFIG
+    POLYBAR_CONFIG,
+    NVIM_CONFIG
 ]
 
 option = `echo "#{options.join("|")}" | rofi -sep '|' -dmenu -i | xargs -r echo`.strip
