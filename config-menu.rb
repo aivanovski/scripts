1bd090fdb40dab42cb179adeaeea897cef08b5f8
Aleksey Ivanovsky <alexei.ivanovski@gmail.com>
2022-03-30 07:55:55 +0200

    Update config-menu.rb script

--- config-menu.rb
+++ config-menu.rb
@@ -2,25 +2,19 @@
 
 # Launch Rofi with selection of config files to edit
 
-EDITOR = "code"
-
-BASHRC = "~/.bashrc"
-VIMRC = "~/.vimrc"
-I3_CONFIG = "~/.config/i3/config"
-POLYBAR_CONFIG = "~/.config/polybar/config.ini"
-NVIM_CONFIG = "~/.config/nvim"
+EDITOR = 'nvim'.freeze
 
 options = [
-    BASHRC,
-    VIMRC,
-    I3_CONFIG,
-    POLYBAR_CONFIG,
-    NVIM_CONFIG
+  '~/.bashrc',
+  '~/.vimrc',
+  '~/.config/i3/config',
+  '~/.config/polybar/config.ini',
+  '~/.config/nvim'
 ]
 
-option = `echo "#{options.join("|")}" | rofi -sep '|' -dmenu -i | xargs -r echo`.strip
+option = `echo "#{options.join('|')}" | rofi -sep '|' -dmenu -i | xargs -r echo`.strip
 
 if !option.empty?
-    path = option.gsub("~", "$HOME")
-    `exec #{EDITOR} "#{path}"`
+  path = option.gsub('~', '$HOME')
+  `terminator  --command '#{EDITOR} #{path}'`
 end
