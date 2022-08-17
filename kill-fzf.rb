b73b07d20459dee51182c832532b1ff17263accd
Aleksey Ivanovsky <alexei.ivanovski@gmail.com>
2022-08-17 15:51:07 +0200

    Update kill-fzf.rb to support macOS

--- kill-fzf.rb
+++ kill-fzf.rb
@@ -2,7 +2,17 @@
 
 # Show list of processes with FZF and kills selected
 
-pid = `ps --user "$(id -u)" -o pid,time,cmd | fzf -m | awk '{print $1}'`.strip
+platform = `uname | head -1`.strip
+
+case platform
+when 'Darwin'
+  pid = `ps -u "$(id -u)" -o pid,time,command | fzf -m | awk '{print $1}'`.strip
+when 'Linux'
+  pid = `ps --user "$(id -u)" -o pid,time,cmd | fzf -m | awk '{print $1}'`.strip
+else
+  puts 'Unsupported Operation System'
+  exit 1
+end
 
 if !pid.empty? && pid != 'PID'
   `kill -9 #{pid}`
