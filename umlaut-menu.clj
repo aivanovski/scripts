888c0fd799c09cd9d74da0b7e5642a50e03424d4
Aleksey Ivanovsky <alexei.ivanovski@gmail.com>
2023-07-21 22:23:10 +0200

    Update umlaut-menu.clj

--- umlaut-menu.clj
+++ umlaut-menu.clj
@@ -15,13 +15,15 @@
       (str/trim result)
       (System/exit 0))))
 
-(def data {"1 - ä" "U00E4",
-           "2 - Ä" "U00C4",
-           "3 - ö" "U00F6",
-           "4 - Ö" "U00D6",
-           "5 - ü" "U00FC",
-           "6 - Ü" "U00DC",
-           "7 - ß" "U00DF"})
+; Uppercase letter codes:
+; "2 - Ä" "U00C4",
+; "4 - Ö" "U00D6",
+; "6 - Ü" "U00DC",
+
+(def data {"1 - ä (a)" "U00E4",
+           "2 - ö (o)" "U00F6",
+           "3 - ü (u)" "U00FC",
+           "4 - ß (s)" "U00DF"})
 
 (def entries (str/join "|" (map #(first %) data)))
 (def key (run (format COMMAND entries)))
