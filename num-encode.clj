3ac3483b2b163ecc9ca9edddeb3d63ef4ef57df0
Aleksey Ivanovsky <alexei.ivanovski@gmail.com>
2023-10-23 21:20:40 +0200

    Refactor gen-pass.clj

--- num-encode.clj
+++ num-encode.clj
@@ -3,39 +3,46 @@
 (require '[clojure.string :as str]
          '[babashka.cli :as cli])
 
-(def info ["Utility to transform number into passphrase depend on provided vocabulary."
-           "The process of transformatio is a simple substition of each digit by corresponding word from vocabulary."
+(def info ["Utility that encodes number into passphrase depend on specified phrase."
+           "The process of encoding is a simple substition of each digit by corresponding word from the phrase."
            ""
            "USAGE:"
-           "    gen-pass [OPTION] [OPTION-VALUE]"
+           "    num-encode [OPTION] [OPTION-VALUE]"
            ""
            "OPTIONS:"
            "    -n, --number              The number to transform, for example 01234"
-           "    -v, --vocabulary          The path to file with list of words separated by new line."
-           "                                  The first word is assigned index 0"
+           "    -p, --phrase              The phrase each word of which is used to encode the number."
+           "                                  The first word of phrase is assigned index 0."
            "    -h, --help                Print help information"])
 
 (def cli-options
-  {:coerce {:vocabulary :string
+  {:coerce {:phrase :string
             :number :string
             :help :boolean}
-   :alias {:v :vocabulary
+   :alias {:p :phrase
            :n :number
            :h :help} })
+
+(def excluded-words #{"the" "and" "for" "but" "nor" "not"})
 
 (defn print-info
   []
   (doseq [line info]
       (println line)))
 
-(defn load-vocabulary
-  [path]
-  (let [content (slurp path)
-        lines (->> (str/split content #"\n")
-                   (map #(str/trim %))
-                   (filter #(not (empty? %))))
-        word-count (count lines)
-        vocabulary (zipmap (range 0 word-count) lines)]
+(defn phrase-to-words
+  [phrase]
+  (->> (str/split phrase #" ")
+       (filter (fn [word]
+                 (and
+                   (not (empty? word))
+                   (> (count word) 2)
+                   (not (contains? excluded-words (str/lower-case word))))))))
+
+(defn words-to-vocabulary
+  [words]
+  (let [word-count (count words)
+        vocabulary (zipmap (range 0 word-count) words)]
     vocabulary))
 
 (defn generate-password
@@ -61,8 +68,8 @@
       (System/exit 1))
 
     (let [number (:number arguments)
-          vocabulaty-path (:vocabulary arguments)
-          vocabulary (load-vocabulary vocabulaty-path)
+          words (phrase-to-words (:phrase arguments))
+          vocabulary (words-to-vocabulary words)
           password (generate-password number vocabulary)]
 
       (println password))))
